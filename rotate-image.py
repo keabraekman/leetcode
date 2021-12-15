2e751ae7dfb198dadf2a7aa122a4c521c439363c
keabraekman <kea.braekman@mail.com>
2021-12-15 15:47:05 -0800

    started working on rotate image

--- rotate-image.py
+++ rotate-image.py
@@ -12,15 +12,64 @@
 #                  [8,5,2],
 #                  [9,6,3]]
 
-def rotate(matrix):
-    n = len(matrix)
-    i = 1
-    steps = 0
-    while(n > i):
-        steps += n-i
-        i += 2
+# You do not return anything. You need to modify the matrix in place!!
 
-    for i in steps:
-        
-        
-    return 0
+# Solution that returns a new matrix
+def rotate(matrix):
+    answer = []
+    for y in range(len(matrix)):
+        # 20, 10, 00, 21, 11, 01, 22, 12, 02, xy
+        row = []
+        for i in range(len(matrix)):
+            x = len(matrix) - 1 - i
+            row.append(matrix[x][y])
+        answer.append(row)
+    return answer
+
+
+test = [[1,2,3],[4,5,6],[7,8,9]]
+
+# print(rotate(test))
+
+
+
+# Input: matrix = [[1,2,3],
+#                  [4,5,6],
+#                  [7,8,9]]
+
+# Output: [        [7,4,1],
+#                  [8,5,2],
+#                  [9,6,3]]
+
+
+# Input: matrix = [[1,2,3,4],
+#                  [5,6,7,8],
+#                  [9,0,1,2],
+#                  [3,4,5,6]]
+
+# Output: matrix = [[3,9,5,1],
+#                   [4,0,6,2],
+#                   [5,1,7,3],
+#                   [6,2,8,4]]
+
+def rotate(matrix):
+    # 00, 02, 22, 20
+    # 01, 12, 21, 10
+
+    # 00, 03, 33, 30
+    # 01, 13, 32, 20
+    # 02, 23, 31, 10
+    # 11, 12, 22, 21
+
+    # xy. x = (previous) y
+    # y = 0,3,3,0
+    # y = 1,3,2,0
+    # y = 2,3,1,0
+    # y = 1,2,2,1
+    
+    return 
+
+
+
+
+
