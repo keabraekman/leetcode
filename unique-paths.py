05dc5b940a2ed71b42e890c6c7212a4dfe77cac9
keabraekman <kea.braekman@mail.com>
2022-01-19 22:10:44 -0800

    unique paths

---
+++ unique-paths.py
@@ -0,0 +1,34 @@
+# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+
+def uniquePaths(m, n):
+    grid = []
+    # firstRow = [1]*n
+    # grid.append(firstRow)
+    for i in range(m):
+        row = [1]*n
+        grid.append(row)
+        for j in range(n):
+            if i > 0 and j < n-1:
+                grid[i][j] = grid[i-1][j] + grid[i][j+1]
+    return grid
+
+
+grid = uniquePaths(3,7)
+
+for i in range(len(grid)):
+    print(grid[i])
+
+
+def uniquePaths(m, n):
+    row = [1]*n
+    for i in range(m-1):
+        newRow = [1]*n
+        for j in range(n-2,-1,-1):
+            newRow[j] = newRow[j+1]+row[j]
+        row = newRow
+    return row[0]
+
+print(uniquePaths(7,3))
