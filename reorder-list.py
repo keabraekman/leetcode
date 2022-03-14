eb29af516825777d52911aeea85119c1e378f316
keabraekman <kea.braekman@mail.com>
2022-03-13 19:17:31 -0700

    reorder list

--- reorder-list.py
+++ reorder-list.py
@@ -47,3 +47,24 @@ def reorderList(head):
 
 
 
+def reorderList(head):
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    second = slow.next
+    prev = slow.next = None
+    
+    while second:
+        tmp = second.next
+        second.next = prev
+        prev = second
+        second = tmp
+        
+    first, second = head, prev
+    while second:
+        tmp1, tmp2 = first.next, second.next
+        first.next = second
+        first = tmp1
+        second.next = first
+        second = tmp2
