9841746ac69036dde1212f29b637f4e03ad2bc80
keabraekman <kea.braekman@mail.com>
2022-03-14 18:25:27 -0700

    spiral matrix

--- spiral-matrix.py
+++ spiral-matrix.py
@@ -34,4 +34,81 @@ def spiralOrder(matrix):
     return result
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# print(spiralOrder(matrix))
+
+
+
+
+def spiralOrder(matrix):
+    res = []
+    left, right, top, bottom = 0, len(matrix[0]), 0, len(matrix)
+    while len(res) < len(matrix)*(len(matrix[0])):
+        for i in range(left, right):
+            res.append(matrix[top][i])
+        top += 1
+        for i in range(top, bottom):
+            res.append(matrix[i][right-1])
+        right -= 1
+
+        if left >= right or top >= bottom:
+            break
+
+        for i in range(right-left):
+            res.append(matrix[bottom-1][right-1-i])
+        bottom -= 1
+        for i in range(bottom-top):
+            res.append(matrix[bottom-1-i][left])
+        left += 1
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def spiralOrder(matrix):
+    res, left, right, top, bottom = [], 0, len(matrix[0]), 0, len(matrix)
+    while len(res) < len(matrix)*len(matrix[0]):
+        for i in range(left, right):
+            res.append(matrix[top][i])
+        top += 1
+        for i in range(top, bottom):
+            res.append(matrix[i][right-1])
+        right -= 1
+        
+        if left >= right or top >= bottom:
+            break
+
+        for i in range(right-left):
+            res.append(matrix[bottom-1][right-1-i])
+        bottom -= 1
+        for i in range(bottom-top):
+            res.append(matrix[bottom-1-i][left])
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 print(spiralOrder(matrix))
