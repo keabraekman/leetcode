2f4a17b7de8608f80cb65803b58c4ddc7b08b7ca
keabraekman <kea.braekman@mail.com>
2022-03-17 18:54:07 -0700

    reverse bits

--- reverse-bits.py
+++ reverse-bits.py
@@ -31,3 +31,34 @@ def reverseBits(n):
 # n = 0000001
 
 # print(reverseBits(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def reservebits(n):
+    ans = 0
+    for i in range(32):
+        bit = (n>>i) & 1
+        ans = ans | (bit << 31-i)
+    return ans
