68d56c3c1664700018e8e4e435adacfc6bc8b8aa
keabraekman <kea.braekman@mail.com>
2022-03-23 11:38:26 -0700

    longest common subsequence

--- coin-change.py
+++ coin-change.py
@@ -67,3 +67,16 @@ def coinChange(coins, amount):
         return dp[amount]
     else:
         return -1
+
+
+def coinChange(coins, amount):
+    dp = [amount+1]*(amount+1)
+    dp[0] = 0
+    for a in range(1,amount+1):
+        for c in coins:
+            if a-c <= 0:
+                dp[a] = min(dp[a], 1+dp[a-c])
+    if dp[amount] < amount+1:
+        return dp[amount]
+    else:
+        return -1
