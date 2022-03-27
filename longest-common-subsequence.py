7c7e795838d0862294d28197498ea6b5253edaa7
keabraekman <kea.braekman@mail.com>
2022-03-26 21:58:07 -0700

    house robber 2

--- longest-common-subsequence.py
+++ longest-common-subsequence.py
@@ -47,3 +47,41 @@ def longestCommonSubsequence(text1, text2):
 text1 = 'abcde'
 text2 = 'ace'
 print(longestCommonSubsequence(text1, text2))
+
+
+
+
+def longestCommonSubsequence(text1, text2):
+    dp = []
+    for i in range(len(text1)+1):
+        dp.append([0]*(len(text2)+1))
+    for i in range(len(text1)-1, -1, -1):
+        for j in range(len(text2)-1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = dp[i+1][j+1]+1
+            else:
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+    for i in range(len(dp)):
+        print(dp[i])
+    return dp[0][0]
+
+
+longestCommonSubsequence(text1, text2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- word-break.py
+++ word-break.py
@@ -55,3 +55,22 @@ def wordBreak(s,wordDict):
             if dp[i]:
                 break
     return dp[0]
+
+
+
+
+
+
+
+
+
+def wordBreak(s, wordDict):
+    dp = [False]*(len(s)+1)
+    dp[len(s)] = True
+    for i in range(len(s)-1, -1, -1):
+        for w in wordDict:
+            if (i+len(w) <= len(s) and s[i:i+len(w)] == w):
+                dp[i] = dp[i+len(w)]
+            if dp[i]:
+                break
+    return dp[0]
